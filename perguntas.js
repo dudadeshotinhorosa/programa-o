df8e80ac63ea0529636b600bb8c777b1cc3619f8
dudadeshotinhorosa <143123677+dudadeshotinhorosa@users.noreply.github.com>
2024-10-09 08:46:11 -0300

    jhuj

---
+++ perguntas.js
@@ -0,0 +1,100 @@
+criaCartao(
+    'matemática',
+    'como se calcula a soma dos termos de uma P.A?',
+    'Utiliza-se a fórmula: ((a1+an)*r)/2'
+)
+criaCartao(
+    'robotica',
+    'Computador é software ou hardware?',
+    'hardware'
+)
+criaCartao(
+    'quimica',
+    'Qual a formula do alceno?',
+    'CnH2n'
+)
+criaCartao(
+    'Ciencias',
+    'Normalmente, quantos litros de sangue uma pessoa tem? Em média, quantos são retirados numa doação de sangue??',
+    'Tem entre 4 a 6 litros'
+)
+criaCartao(
+    'Historia',
+    'De onde é a invenção do chuveiro elétrico?',
+    'Brasil'
+)
+criaCartao(
+    'geografia',
+    'Quais o menor e o maior país do mundo?',
+    'Vaticano e Rússia.'
+)
+criaCartao(
+    'portuguesa',
+    ' Qual o grupo em que todas as palavras foram escritas corretamente?',
+    ' Asterisco, beneficente, meteorologia, entretido.'
+)
+criaCartao(
+    'historia',
+    'Qual o livro mais vendido no mundo a seguir à Bíblia?',
+    'Dom Quixote.'
+)
+criaCartao(
+    'quimica',
+    'Atualmente, quantos elementos químicos a tabela periódica possui?',
+    '118'
+)
+criaCartao(
+    'geografia',
+    'Quais os países que têm a maior e a menor expectativa de vida do mundo?',
+    'Japão e Serra Leoa.'
+)
+criaCartao(
+    'ingles',
+    'O que a palavra legend significa em português?',
+    'lenda'
+)
+criaCartao(
+    'ciencias',
+    'Qual a velocidade da luz?',
+    ' 299 792 458 metros por segundo (m/s).'
+)
+criaCartao(
+    'geografia',
+    'Qual a montanha mais alta do Brasil?',
+    'pico da neblina'
+)
+criaCartao(
+    'historia',
+    'Em que período da pré-história o fogo foi descoberto?',
+    'peleolitico'
+)
+varcriaCartao(
+    'educação fisica',
+    'Qual a altura da rede de vôlei nos jogos masculino e feminino?',
+    ' 2,43 m e 2,24 m.'
+)
+criaCartao(
+    'ingles',
+    'Qual a tradução da frase “Fabiano cogió su saco antes de salir”?',
+    'Fabiano pegou seu paletó antes de sair.'
+)
+criaCartao(
+    'ciencias',
+    'Quanto tempo a luz do Sol demora para chegar à Terra?',
+    '8 minutos'
+)
+criaCartao(
+    'artes',
+    'Quem pintou "Guernica"?',
+    'Pablo picasso'
+)
+criaCartao(
+    'geografia',
+    'Quais as duas datas que são comemoradas em novembro?',
+    'Proclamação da República e Dia Nacional da Consciência Negra.'
+)
+criaCartao(
+    'Ed fisica',
+    'Qual o número mínimo de jogadores em cada time numa partida de futebol?',
+7    ''
+)
